5bfe902d2a11f2ca1b06709703fbf4b7db21d91d
Mateus Siqueira Salomão <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-09-11 14:21:24 -0300

    primeira atualização

---
+++ ProjetoFinalBloco1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoFinalBloco1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
